4cf7d14042a423f4c08f43135e2b9aee429ee68b
Mina Hany <mina.hany@transformus.co>
2024-09-14 16:36:12 -0700

    feat: setup basic MySQL configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,26 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'mysql',
+        host: config.get('DB_HOST'),
+        port: config.get('DB_PORT'),
+        database: config.get('DB_NAME'),
+        username: config.get('DB_USERNAME'),
+        password: config.get('DB_PASSWORD'),
+        synchronize: true,
+        autoLoadEntities: true,
+      }),
+    }),
+  ],
   controllers: [],
   providers: [],
 })
